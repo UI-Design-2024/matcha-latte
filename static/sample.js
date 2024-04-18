3bb04ec0d04f59b4e5e4b9b41d86e8faf33d1d35
kent12112 <kentoyanagishita@gmail.com>
2024-04-18 18:12:34 -0400

    Implemented to-quiz button

--- static/sample.js
+++ static/sample.js
@@ -1,14 +1,20 @@
 let currentStep = 0;
 const maxSteps = 9;
-
+var added = false
 function navigate(direction) {
 	const current = document.getElementById('step' + currentStep);
 	currentStep += direction;
-
 	if (currentStep > maxSteps) {
 		currentStep = maxSteps;
+		if (!added){
+		$(".to-quiz").html('<a href="/quiz_home"><button class="quiz-button">Quiz</button></a>')
+		added = true
+		}
 	} else if (currentStep < 1) {
 		currentStep = 1;
+	}else{
+		added = false
+		$(".to-quiz").html("")
 	}
 
 	const next = document.getElementById('step' + currentStep);
